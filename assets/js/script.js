0f5798f10a3358e9251e7788278058cb69dd1d0b
wildarte <wildarte10@gmail.com>
2024-01-26 17:28:20 -0300

    home

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         margin:10,
         nav:true,
         autoplay:true,
-        autoplayTimeout:2000,
+        autoplayTimeout: 4000,
         autoplayHoverPause:true,
         responsive:{
             0:{
@@ -18,4 +18,35 @@ $(document).ready(function(){
             }
         }
     });
+
+
+    const btn_menu = document.querySelector('.btn_menu');
+    const nav_menu = document.querySelector('nav.menu');
+    const btn_close_menu = document.querySelector('.btn_close_menu');
+
+    btn_menu.addEventListener('click', () => {
+        nav_menu.classList.add('open_menu')
+    });
+
+    btn_close_menu.addEventListener('click', () => {
+        nav_menu.classList.remove('open_menu')
+    });
+
+
+    const btn_search = document.querySelector('.btn_search');
+    const over_search = document.querySelector('.over_search');
+    const btn_close_search = document.querySelector('.btn_close_search');
+
+    btn_search.addEventListener('click', () => {
+
+        over_search.classList.add('open_search')
+
+    });
+
+    btn_close_search.addEventListener('click', () => {
+
+        over_search.classList.remove('open_search')
+
+    });
+
 });
