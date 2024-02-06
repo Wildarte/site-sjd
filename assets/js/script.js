7e7baaf0813514ff6996545f07370ba0dc75597d
wildarte <wildarte10@gmail.com>
2024-02-06 20:32:30 -0300

    page servicos

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,27 @@ $(document).ready(function(){
         }
     });
 
+    $(".destaques_page").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
 
     const btn_menu = document.querySelector('.btn_menu');
     const nav_menu = document.querySelector('nav.menu');
